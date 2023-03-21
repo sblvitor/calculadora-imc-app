f29503440ce0d32f43b5efe135677d6af5a309d8
Vitor <sblvitor@gmail.com>
2023-03-20 20:22:23 -0400

    refactor: imc model

--- lib/models/imc.dart
+++ lib/models/imc.dart
@@ -1,30 +1,9 @@
-import 'package:flutter/cupertino.dart';
 
 class IMC {
-  final String _id = UniqueKey().toString();
-  double _peso = 0;
-  double _altura = 0;
-  double _imc = 0;
+  int id = 0;
+  double peso = 0;
+  double altura = 0;
+  double imc = 0;
 
-  IMC(this._peso, this._altura, this._imc);
-
-  String get id => _id;
-
-  double get peso => _peso;
-
-  set peso(double peso) {
-    _peso = peso;
-  }
-
-  double get altura => _altura;
-
-  set altura(double altura) {
-    _altura = altura;
-  }
-
-  double get imc => _imc;
-
-  set imc(double imc) {
-    _imc = imc;
-  }
+  IMC(this.peso, this.altura, this.imc);
 }
